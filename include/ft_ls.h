d54905cd63903b046061c5d91d40425eedbc6593
kcharla <kcharla@student.21-school.ru>
2020-03-08 21:52:38 +0300

    Cmake config added

---
+++ include/ft_ls.h
@@ -0,0 +1,12 @@
+#ifndef FT_LS_H
+# define FT_LS_H
+
+#include <sys/types.h>
+#include <dirent.h>
+
+typedef struct		s_ft_ls
+{
+	int		code;
+}					t_ft_ls;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "ft_ls.h"
+
+int		main()
+{
+	t_ft_ls		data;
+
+	return (0);
+}
